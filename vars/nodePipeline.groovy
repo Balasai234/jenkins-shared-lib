d511069560ddbf9d2989d4fce45763ac4ff2f81d
Balasai234 <balasaicheni7@gmail.com>
2026-03-02 12:32:26 +0530

    Implement Jenkins pipeline with build and deploy stages

--- vars/nodePipeline.groovy
+++ vars/nodePipeline.groovy
@@ -1,3 +1,4 @@
+/*
 def call(Map config) {
 
     pipeline {
@@ -20,6 +21,52 @@ def call(Map config) {
             stage('Docker Build') {
                 steps {
                     sh "docker build -t ${config.appName}:latest ."
+                }
+            }
+
+        }
+    }
+}
+*/
+def call(Map config) {
+
+    pipeline {
+        agent { label 'AGENT-1' }
+
+        parameters {
+            choice(name: 'ENV', choices: config.environments, description: 'Select environment')
+        }
+
+        stages {
+
+            stage('Build') {
+                steps {
+                    sh 'npm install'
+                }
+            }
+
+            stage('Docker Build') {
+                steps {
+                    sh "docker build -t ${config.appName}:${params.ENV} ."
+                }
+            }
+
+            stage('Deploy') {
+                steps {
+                    script {
+
+                        // Dynamically construct credential ID
+                        def dbCredentialId = "db-url-${params.ENV}"
+
+                        withCredentials([string(credentialsId: dbCredentialId, variable: 'DB_URL')]) {
+
+                            sh '''
+                                echo "Deploying to ${ENV}"
+                                echo "DB_URL is injected securely"
+                                node app.js
+                            '''
+                        }
+                    }
                 }
             }
 
